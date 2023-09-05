bd1062a28a949039f0e71dedfad97893414ebccf
maperz22 <dabrowski.racing.35@gmail.com>
2023-09-06 00:28:55 +0200

    Classes replaced by Records

--- src/main/java/com/maperz/githubreposearch/dto/github/Branch.java
+++ src/main/java/com/maperz/githubreposearch/dto/github/Branch.java
@@ -1,8 +1,3 @@
 package com.maperz.githubreposearch.dto.github;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 public record Branch(String name, Commit commit) {
 }
--- src/main/java/com/maperz/githubreposearch/dto/userRepos/UserRepo.java
+++ src/main/java/com/maperz/githubreposearch/dto/userRepos/UserRepo.java
@@ -1,15 +1,10 @@
 package com.maperz.githubreposearch.dto.userRepos;
 
 import com.maperz.githubreposearch.dto.github.Branch;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-public class UserRepo {
-    private String name;
-    private String owner;
-    private List<Branch> branches;
+
+
+public record UserRepo(String name, String owner, List<Branch> branches) {
 }
